1b00e28b0e76365050213fcd96e5e0c64e726a8e
dariazh <oduvashk@mail.ru>
2019-01-13 21:50:58 +0200

    step1

--- app/Controllers/Http/TypeController.js
+++ app/Controllers/Http/TypeController.js
@@ -1,44 +1,38 @@
 'use strict'
+const Type = use('App/Models/Type');
 
 class TypeController {
+
     async index () {
-        return [
-            {
-                typeController: "index"
-            }
-        ]
+       return Type.findAllTypes()
     }
 
     async store() {
-        return [
-            {
-                typeController: "store"
-            }
-        ]
+        const data = {
+            'id': 'phone'
+        }
+
+        return Type.createByTypesId(data)
     }
 
-    async show() {
-        return [
-            {
-                typeController: "show"
-            }
-        ]
+    async show({ params}) {
+        const {id} = params
+        return Type.findByTypesId(id);
     }
 
-    async update () {
-        return [
-            {
-                typeController: "update"
-            }
-        ]
+
+    async update ({ params}) {
+        const {id} = params
+        const data = {
+            'id': id,
+            'update_id': 'watch'
+        }
+        return Type.updateByTypesId(data)
     }
 
-    async destroy () {
-        return [
-            {
-                typeController: "destroy"
-            }
-        ]
+    async destroy ({params,response}) {
+        const { id } = params;
+        return Type.deleteByTypesId(id, response)
     }
 }
 
