1b1dbfdf35471c34f96be7e053e99492d2182f00
dariazh <oduvashk@mail.ru>
2019-01-15 23:19:05 +0200

    step2

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -3,32 +3,42 @@
 const Product = use('App/Models/Product');
 
 class ProductController {
-    async index () {
-        return Product.findAllProducts()
+    async index ({ request }) {
+        return Product.findAll(request.only(['filters', 'order', 'sort']))
     }
 
-    async store({ params, request }) {
+    async store({ request, response }) {
         const data = request.only(['name', 'price', 'type_id', 'user_id', 'created_at'])
-        return Product.createByProductsId(data)
+        const attr = request.input('attribute')
+        const newProduct = await Product.createNew({
+            data: data,
+            attr: attr
+        })
+
+        return response.status(201).json(newProduct);
+
     }
 
-    async show({ params }) {
+    async show({ params, response }) {
         const {id} = params
-        return Product.findByProductsId(id);
+
+        const showProd = await Product.findById(id);
+        return response.json(showProd);
     }
 
-    async update ({ params}) {
+    async update ({ params, request, response}) {
+        const data = request.only(['name', 'price', 'type_id', 'user_id', 'created_at'])
         const {id} = params
-        const data = {
-            'id': id,
-            'update_id': 'OillPl'
-        }
-        return Product.updateByProductsId(data)
+        const attr = request.input('attribute')
+
+        const showUpdateProd = await Product.updateById({id: id, data: data, attr:attr})
+        return response.json(showUpdateProd);
     }
 
     async destroy ({params, response}) {
         const { id } = params;
-        return Product.deleteByProductsId(id, response)
+        const delProd = await Product.deleteById(id)
+        return response.json(delProd);
     }
 
 }
