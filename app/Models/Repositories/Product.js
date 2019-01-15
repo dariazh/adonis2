f59e091fb8e874dcbdb28623e04e790fb22c7e13
dariazh <oduvashk@mail.ru>
2019-01-15 23:19:05 +0200

    step2

--- app/Models/Repositories/Product.js
+++ app/Models/Repositories/Product.js
@@ -1,44 +1,49 @@
 class Product {
-    static async findAllProducts() {
-        const result = await this.all()
+    static async findAll({ filters = null, order = 'id', sort = 'desc'}) {
+        const filtersFields = ['type_id', 'user_id', 'name'];
+        return this.query().where(function(){
+            if(filters && typeof filters === 'object'){
+                Object.keys(filters).map(field => {
+                    if(filtersFields.includes(field)){
+                        this.where(field, filters[field])
+                    }
+                })
+            }
+        })
+            .orderBy(order, sort)
+            .fetch();
+    }
+
+    static async findById(id) {
+        const find_product = await this.findOrFail(id)
+        await find_product.loadMany(['attributes', 'type', 'users'])
+
+        return find_product
+    }
+
+    static async updateById({id, params, attr}) {
+        const result = await this.findOrFail(id)
+        result.merge(params)
+        await result.save()
+        await result.attributes().pivotQuery().where('attribute_id', attr).update({value: attr})
+        await result.loadMany(['attributes', 'type', 'users'])
+
         return result
     }
 
-    static async findByProductsId(id) {
-        const  find_product = await this.findOrFail(id)
-        const result  = await find_product.attributes().fetch()
-
-        return result;
-    }
-
-    static async updateByProductsId(params) {
-        const {id, update_id} = params
-
-        const  find_product = await this.findOrFail(id)
-        find_product.merge({name:update_id})
-        find_product.save()
-
-        const result  = await find_product.attributes().fetch()
-
+    static async createNew({data, attr}){
+        const  result = await this.create(data)
+        await result.attributes().attach(attr, row => {
+            row.value = attr;
+        })
         return result
     }
 
-    static async createByProductsId(params){
-        const {name} = params
-        await this.create(params)
-
-        const  find_result = await this.findByOrFail('name', name)
-        const relation_result  = await find_result.attributes().fetch()
-
-
-        return relation_result
-    }
-
-    static async deleteByProductsId(id,response){
+    static async deleteById(id){
         const result = await this.findOrFail(id);
         await result.delete();
 
-        return response.json({msg: 'Ok'});
+        return result
     }
 
 }
